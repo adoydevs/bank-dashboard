922a05e7944e13c06dc17fe5c42cdde7e5a73f3b
adoydevs <imranadoy@gmail.com>
2023-08-03 10:31:35 +0600

    Add

--- script/sricpt.js
+++ script/sricpt.js
@@ -1,5 +1,1 @@
-const headOne = document.getElementById('head-color');
-headOne.style.color = "blue";
-
-const headTwo = document.getElementById('head-two');
-headTwo.style.color = "green";
+alert("Hello");
