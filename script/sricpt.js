01578416fc8e91ad02385440eaecd5b87438f81c
adoydevs <imranadoy@gmail.com>
2023-08-07 19:34:40 +0600

    add

--- script/sricpt.js
+++ script/sricpt.js
@@ -2,7 +2,7 @@ document.getElementById('btn-submit').addEventListener('click' , function(){
     
     const userName = document.getElementById('user-name');
    const name = userName.value;
-   
+
     const emailField = document.getElementById('user-email');
    const emailF = emailField.value;
    
@@ -10,5 +10,13 @@ document.getElementById('btn-submit').addEventListener('click' , function(){
    const passwordF = passField.value;
 
    
-   console.log( emailF , passwordF , name);
+//    console.log( emailF , passwordF , name);
+
+
+    if( emailF === "adoy@gmail.com" && passwordF === "Adoy2050"){
+        console.log('Valid User');
+    }
+    else{
+        console.log('Invalid User');
+    }
 })
