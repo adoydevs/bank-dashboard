de9ffb1911c7d89eec87c14f184b2eb6a941a9dc
adoydevs <imranadoy@gmail.com>
2023-08-07 19:24:34 +0600

    add

--- script/sricpt.js
+++ script/sricpt.js
@@ -1,5 +1,14 @@
-console.log('Click me here');
-
 document.getElementById('btn-submit').addEventListener('click' , function(){
-    console.log( 'Hello I am Here');
+    
+    const userName = document.getElementById('user-name');
+   const name = userName.value;
+   
+    const emailField = document.getElementById('user-email');
+   const emailF = emailField.value;
+   
+   const passField = document.getElementById('user-pass');
+   const passwordF = passField.value;
+
+   
+   console.log( emailF , passwordF , name);
 })
